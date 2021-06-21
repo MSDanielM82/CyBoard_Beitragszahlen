30d6ffe26ed39774e380eb795769808688e1c444
DanielM <github@die-matticks.de>
2021-06-21 13:04:43 +0200

    Initiale Einrichtung

---
+++ CyBoard_Beitragszahlen/CyBoard_Beitragszahlen.Designer.cs
@@ -0,0 +1,187 @@
+﻿
+namespace CyBoard_Beitragszahlen
+{
+    partial class CyBoard_Beitragszahlen
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CyBoard_Beitragszahlen));
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.btn_StartSeite1 = new System.Windows.Forms.Button();
+            this.lbl_fortschritt = new System.Windows.Forms.Label();
+            this.btn_StartSeiteX = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.dateiToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.beendenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.infoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(12, 36);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.ScriptErrorsSuppressed = true;
+            this.webBrowser1.Size = new System.Drawing.Size(1000, 668);
+            this.webBrowser1.TabIndex = 0;
+            // 
+            // btn_StartSeite1
+            // 
+            this.btn_StartSeite1.Location = new System.Drawing.Point(1027, 26);
+            this.btn_StartSeite1.Name = "btn_StartSeite1";
+            this.btn_StartSeite1.Size = new System.Drawing.Size(215, 86);
+            this.btn_StartSeite1.TabIndex = 1;
+            this.btn_StartSeite1.Text = "Ab Seite 1 starten";
+            this.btn_StartSeite1.UseVisualStyleBackColor = true;
+            this.btn_StartSeite1.Click += new System.EventHandler(this.btn_StartSeite1_Click);
+            // 
+            // lbl_fortschritt
+            // 
+            this.lbl_fortschritt.AutoSize = true;
+            this.lbl_fortschritt.Location = new System.Drawing.Point(1023, 613);
+            this.lbl_fortschritt.Name = "lbl_fortschritt";
+            this.lbl_fortschritt.Size = new System.Drawing.Size(51, 20);
+            this.lbl_fortschritt.TabIndex = 5;
+            this.lbl_fortschritt.Text = "label1";
+            // 
+            // btn_StartSeiteX
+            // 
+            this.btn_StartSeiteX.Location = new System.Drawing.Point(1027, 184);
+            this.btn_StartSeiteX.Name = "btn_StartSeiteX";
+            this.btn_StartSeiteX.Size = new System.Drawing.Size(215, 89);
+            this.btn_StartSeiteX.TabIndex = 6;
+            this.btn_StartSeiteX.Text = "Ab Seite 1 starten";
+            this.btn_StartSeiteX.UseVisualStyleBackColor = true;
+            this.btn_StartSeiteX.Click += new System.EventHandler(this.btn_StartSeiteX_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(1027, 280);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            9999,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(215, 26);
+            this.numericUpDown1.TabIndex = 7;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.dateiToolStripMenuItem,
+            this.toolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1254, 33);
+            this.menuStrip1.TabIndex = 8;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // dateiToolStripMenuItem
+            // 
+            this.dateiToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.beendenToolStripMenuItem});
+            this.dateiToolStripMenuItem.Name = "dateiToolStripMenuItem";
+            this.dateiToolStripMenuItem.Size = new System.Drawing.Size(69, 29);
+            this.dateiToolStripMenuItem.Text = "Datei";
+            // 
+            // beendenToolStripMenuItem
+            // 
+            this.beendenToolStripMenuItem.Name = "beendenToolStripMenuItem";
+            this.beendenToolStripMenuItem.Size = new System.Drawing.Size(182, 34);
+            this.beendenToolStripMenuItem.Text = "Beenden";
+            this.beendenToolStripMenuItem.Click += new System.EventHandler(this.beendenToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.infoToolStripMenuItem});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(36, 29);
+            this.toolStripMenuItem1.Text = "?";
+            // 
+            // infoToolStripMenuItem
+            // 
+            this.infoToolStripMenuItem.Name = "infoToolStripMenuItem";
+            this.infoToolStripMenuItem.Size = new System.Drawing.Size(146, 34);
+            this.infoToolStripMenuItem.Text = "Info";
+            // 
+            // CyBoard_Beitragszahlen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1254, 716);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.btn_StartSeiteX);
+            this.Controls.Add(this.lbl_fortschritt);
+            this.Controls.Add(this.btn_StartSeite1);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "CyBoard_Beitragszahlen";
+            this.Text = "CyBoard Beitragszahlen";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Button btn_StartSeite1;
+        private System.Windows.Forms.Label lbl_fortschritt;
+        private System.Windows.Forms.Button btn_StartSeiteX;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem dateiToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem beendenToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem infoToolStripMenuItem;
+    }
+}
+
---
+++ CyBoard_Beitragszahlen/CyBoard_Beitragszahlen.cs
@@ -0,0 +1,185 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace CyBoard_Beitragszahlen
+{
+    public partial class CyBoard_Beitragszahlen : Form
+    {
+        string CSVFile = "";
+        string Seiten;
+        string filename;
+        string UserBeitraege;
+        string UserReaktionen;
+        string UserPunkte;
+        string UserTrophaen;
+
+        public CyBoard_Beitragszahlen()
+        {
+            InitializeComponent();
+            numericUpDown1.TextChanged += new EventHandler(NumericUpDown1_ValueChanged); //EventHandler, damit der Button Text aktualisiert wird
+        }
+        private void NumericUpDown1_ValueChanged(Object sender, EventArgs e)
+        {
+            btn_StartSeiteX.Text = "Ab Seite " + numericUpDown1.Value.ToString() + " starten";
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Direkt die Member List anzeigen, zur Überprüfung, ob man eingeloggt ist
+            webBrowser1.Navigate("https://www.moonsault.de/members-list/?pageNo=1&sortField=wbbPosts&sortOrder=DESC"); 
+        }
+
+
+        /// <summary>
+        /// Es wird die Webseite geladen und gewartet, bis der Ladevorgang abgeschlossen wurde
+        /// </summary>
+        /// <param name="URL"></param>
+        public void LoadPage(string URL)
+        {
+            webBrowser1.Navigate(URL);
+            while (webBrowser1.ReadyState != WebBrowserReadyState.Complete)
+            {
+                Application.DoEvents();
+            }
+        }
+        
+
+    public void CreateCSV()
+        {
+            filename = @"c:\cyboard_postingzahlen\export_" + DateTime.Now.ToString("yyyy_MM_dd_HH_mm_ss") + ".csv";
+            using (StreamWriter w = File.AppendText(filename))
+            {
+                CSVFile = "UserID" + ";" + "UserName" + ";" + "UserBeitraege" + ";" + "UserReaktionen" + ";" + "UserPunkte" + ";" + "UserTrophaen" + ";" + "Datum" + ";" + "Uhrzeit" + ";" + "Seite" + ";" + System.Environment.NewLine;
+                w.WriteLine(CSVFile);
+                w.Close();
+            }
+        }
+
+        private void btn_StartSeite1_Click(object sender, EventArgs e)
+        {
+            CreateCSV(); //CSV wird erstellt
+            DurchlaufSeiten(1); //
+            
+        }
+
+        public void ErmittlungSeitenanzahl()
+        {
+
+            var doc = new HtmlAgilityPack.HtmlDocument();
+            doc.LoadHtml(webBrowser1.DocumentText);
+            foreach (HtmlNode seiten in doc.DocumentNode.SelectNodes("//div[@class='paginationBottom']"))
+            {
+                var pageshtml = new HtmlAgilityPack.HtmlDocument();
+                pageshtml.LoadHtml(seiten.InnerHtml);
+                Seiten = pageshtml.DocumentNode.SelectNodes("nav").First().Attributes["data-pages"].Value;
+                MessageBox.Show("Anzahl Seiten:" + Seiten);
+            }
+        }
+        public void DurchlaufSeiten(int pagestart)
+        {
+            ErmittlungSeitenanzahl();
+
+            int page = pagestart;
+            int page_total = int.Parse(Seiten);
+
+            while (page < page_total+1)
+            {
+                LoadPage(@"https://www.moonsault.de/members-list/?pageNo=" + page.ToString() + "&sortField=wbbPosts&sortOrder=DESC");
+                WebseiteAuslesen(page);
+                lbl_fortschritt.Text = page.ToString() + "/" + page_total.ToString();
+                page++;
+
+            }
+            }
+     
+        private void WebseiteAuslesen(int page)
+        { 
+            var doc = new HtmlAgilityPack.HtmlDocument();
+            doc.LoadHtml(webBrowser1.DocumentText);
+            
+            foreach (HtmlNode node in doc.DocumentNode.SelectNodes("//ol[@class='containerList userList']"))
+            {
+                var innerhtmlDoc = new HtmlAgilityPack.HtmlDocument();
+                innerhtmlDoc.LoadHtml(node.InnerHtml);
+
+                var htmlNodes = innerhtmlDoc.DocumentNode.SelectNodes("//li/div[@class='box48']");
+                foreach (var nodes in htmlNodes)
+                {
+                    //Initialisierung der Beiträge, Reaktionen, Trophaen und Punkte
+                    UserBeitraege = "0";
+                    UserReaktionen = "0";
+                    UserTrophaen = "0";
+                    UserPunkte = "0";
+
+                    //Auslesen des Namen und der ID
+                    var tmp_html = new HtmlAgilityPack.HtmlDocument();
+                    tmp_html.LoadHtml(nodes.InnerHtml);
+                    
+                    string UserName = tmp_html.DocumentNode
+                       .SelectNodes("//a")
+                       .First()
+                       .Attributes["title"].Value;
+                    string UserID = tmp_html.DocumentNode
+                        .SelectNodes("//a[@class='userLink username']")
+                        .First()
+                        .Attributes["data-object-id"]
+                        .Value;
+                    
+
+                    //Für jeden DL Knoten werden die Daten ausgelesen. Beiträge, Reaktionen, Punkte, Trophaen
+                    int counter = 0;
+                    foreach (HtmlNode infos in tmp_html.DocumentNode.SelectNodes("//dl[@class='plain inlineDataList small']/dd"))
+                    {
+                        switch (counter)
+                        {
+                            case 0:
+                                UserBeitraege = infos.InnerText;
+                                break;
+                            case 1:
+                                UserReaktionen = infos.InnerText;
+                                break;
+                            case 2:
+                                UserPunkte = infos.InnerText;
+                                break;
+                            case 3:
+                                UserTrophaen = infos.InnerText;
+                                break;
+                        }
+                        counter++;
+                    }
+
+                    //CSV Schreiben
+                    using (StreamWriter w = File.AppendText(filename))
+                    {
+                        CSVFile = UserID + ";" + UserName + ";" + UserBeitraege.Replace(".", "") + ";" + UserReaktionen.Replace(".", "") + ";" + UserPunkte.Replace(".", "") + ";" + UserTrophaen.Replace(".", "") + ";" + DateTime.Now.ToString("d") + ";" + DateTime.Now.ToString("HH:mm:ss") + ";" + page.ToString() + ";" + System.Environment.NewLine;
+                        w.WriteLine(CSVFile);
+                        w.Close();
+                    }
+
+                   
+
+                }
+            }
+
+                
+            
+        }
+      
+
+        private void btn_StartSeiteX_Click(object sender, EventArgs e)
+        {
+            CreateCSV();
+            LoadPage(@"https://www.moonsault.de/members-list/?pageNo=1&sortField=wbbPosts&sortOrder=DESC&letter=");
+            int start = int.Parse(numericUpDown1.Value.ToString());
+            DurchlaufSeiten(start);
+        }
+
+        private void beendenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+    }
+
